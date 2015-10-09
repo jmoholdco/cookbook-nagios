fde86c481aa707b6bdc6a9c9b2844a76986d7784
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-09 13:13:50 -0600

    initial commit of nagios cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'nagios'
+maintainer 'J. Morgan Lieberthal'
+maintainer_email 'j.morgan.lieberthal@gmail.com'
+license 'mit'
+description 'Installs/Configures nagios'
+long_description 'Installs/Configures nagios'
+version '0.1.0'
