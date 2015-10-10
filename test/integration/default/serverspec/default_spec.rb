f7e64b56562d26a4f2a07768b7ee197c6c49ea83
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-09 20:27:34 -0600

    Integration Tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,34 @@
 require 'spec_helper'
 
 RSpec.describe 'nagios::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe file('/usr/local/nagios') do
+    it { is_expected.to be_directory }
+    it { is_expected.to exist }
+  end
+
+  describe service('httpd') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+
+  describe service('nagios') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+
+  describe service('mysql-nagios') do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  describe user('nagios') do
+    it { is_expected.to exist }
+    it { is_expected.to belong_to_group 'nagios' }
+    it { is_expected.to belong_to_group 'nagcmd' }
+  end
+
+  describe user('apache') do
+    it { is_expected.to exist }
+    it { is_expected.to belong_to_group 'nagcmd' }
   end
 end
